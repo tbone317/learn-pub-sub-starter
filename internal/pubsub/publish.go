8b3de25998e4d126c8e76198204ccc6a8139e0e3
Travis Dunbar <travis.j.dunbar@gmail.com>
2026-04-08 17:30:14 -0500

    publishgob

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -1,19 +1,51 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+func PublishJSON[T any](ch *amqp.Channel,
+	exchange,
+	key string,
+	val T) error {
 	dat, err := json.Marshal(val)
 	if err != nil {
 		return err
 	}
-	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        dat,
-	})
+	return ch.PublishWithContext(context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        dat,
+		})
+}
+
+func PublishGob[T any](ch *amqp.Channel,
+	exchange,
+	key string,
+	val T) error {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(val)
+	if err != nil {
+		return err
+	}
+	dat := buf.Bytes()
+	return ch.PublishWithContext(context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/gob",
+			Body:        dat,
+		})
 }
